eb1b2e1af196059f6842e19a407ade6cad308ac6
Ryan C Slivinski <ryan.slivinski@uconn.edu>
2021-08-05 14:18:44 -0400

    Video 47 - start of Section 4

--- src/index.js
+++ src/index.js
@@ -1,11 +1,51 @@
 // import React and ReactDOM libs
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 // Create React component
 const App = () =>
 {
-	return <div>Hello World!</div>
+	return(
+		<div className="ui container comments">
+
+			<ApprovalCard>
+				<div>
+					<h4>WARNING!</h4>
+					Are you sure you want to do this?
+				</div>
+			</ApprovalCard>
+
+			<ApprovalCard>
+				<CommentDetail 
+					author="Sam" 
+					timePosted="Today at 4:20pm" 
+					content="Nice cock!" 
+					avatar={Faker.image.avatar()} 
+				/>
+			</ApprovalCard>
+
+			<ApprovalCard>
+				<CommentDetail 
+					author="Alex" 
+					timePosted="Today at 2:30am" 
+					content="Sick bobs" 
+					avatar={Faker.image.avatar()} 
+				/>
+			</ApprovalCard>
+
+			<ApprovalCard>
+				<CommentDetail 
+					author="Jane" 
+					timePosted="3 days ago" 
+					content="420" 
+					avatar={Faker.image.avatar()} 
+				/>
+			</ApprovalCard>
+		</div>
+	);
 };
 
 // take react component and show to screen
