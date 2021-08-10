1b00731113edf16ee19f0a2ff2e6dbf2701dd1d2
Ryan <ryanslivinski26@gmail.com>
2021-08-10 14:22:42 -0400

    video 136 - Section 12, finished Videos app

--- src/api/unsplash.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: 'https://api.unsplash.com',
-    headers: 
-    {
-        Authorization: 'Client-ID tACPZF0LenUiQsMtlkzebzg7dtJydYPRkv2_rQjPEc4'
-    }
-});
--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,62 @@
 import React from 'react';
-import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
-import ImageList from './ImageList';
+import youtube from '../api/youtube';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component
 {
+    state = 
+    { 
+        videos: [], 
+        selectedVideo: null 
+    };
 
-    state = { images: [] };
-
-    onSearchSubmit = async term =>
+    componentDidMount()
     {
-        const response = await unsplash.get('/search/photos', 
-        {
-            params: { query: term },
+        this.onTermSubmit('Warzone');
+    };
+
+    // queries YouTube API for videos
+    onTermSubmit = async term =>
+    {
+        const response = await youtube.get('/search', {
+            params: {
+                q: term
+            }
         });
 
-        this.setState({ images: response.data.results });
-    }
+        this.setState({
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        });
+    };
+
+    onVideoSelect = video =>
+    {
+        this.setState({ selectedVideo: video });
+    };
 
     render(){
         return(
-            <div className='ui container' style={{marginTop: '10px'}}>
-                <SearchBar onSubmit={this.onSearchSubmit} />
-                <ImageList images={this.state.images} />
+            <div className='ui container'>
+                <SearchBar onFormSubmit={this.onTermSubmit}/>
+                <div className='ui grid'>
+                    <div className='ui row'>
+                        <div className='eleven wide column'>
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className='five wide column'>
+                            <VideoList
+                                onVideoSelect={this.onVideoSelect}
+                                videos={this.state.videos} 
+                            />
+                        </div>
+                    </div>
+                </div> 
             </div>
         );
-    };  
+    };
 }
 
 export default App;
